71019b2f12f09dd95d014a0be91b3933ae2373ef
Bernhard Beschow <bbeschow@cs.tu-berlin.de>
2012-06-06 13:07:02 +0200

    introduce MarbleGLView

--- src/lib/MarbleWidget.cpp
+++ src/lib/MarbleWidget.cpp
@@ -42,7 +42,7 @@
 #include "MarbleMap.h"
 #include "MarbleModel.h"
 #include "MarblePhysics.h"
-#include "MarbleView.h"
+#include "MarbleGLView.h"
 #include "MarbleWidgetInputHandler.h"
 #include "MarbleWidgetPopupMenu.h"
 #include "Planet.h"
@@ -167,7 +167,7 @@ MarbleWidget::MarbleWidget(QWidget *parent)
     QVBoxLayout *vlayout = new QVBoxLayout( this );
     vlayout->setMargin( 0 );
 
-    d->m_view = new MarbleView( &d->m_map, this );
+    d->m_view = new MarbleGLView( &d->m_map, this );
     vlayout->addWidget( d->m_view );
 
     d->construct();
